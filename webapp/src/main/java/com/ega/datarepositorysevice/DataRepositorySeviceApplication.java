42a3a8d0a343b2dd7ee619e912faea0cc57621be
Dilschat <dilschat@yandex.ru>
2019-05-18 15:48:33 +0300

    initial commit

---
+++ webapp/src/main/java/com/ega/datarepositorysevice/DataRepositorySeviceApplication.java
@@ -0,0 +1,13 @@
+package main.java.com.ega.datarepositorysevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRepositorySeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRepositorySeviceApplication.class, args);
+	}
+
+}
