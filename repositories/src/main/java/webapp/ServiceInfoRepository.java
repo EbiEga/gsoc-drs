37e8ed251edde2eac64c54304471b84fb8a25365
Dilschat <dilschat@yandex.ru>
2019-05-28 23:54:50 +0500

    base class for repository changed

--- repositories/src/main/java/webapp/ServiceInfoRepository.java
+++ repositories/src/main/java/webapp/ServiceInfoRepository.java
@@ -1,7 +1,7 @@
 package webapp;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServiceInfoRepository extends CrudRepository<ServiceInfo, String> {
+public interface ServiceInfoRepository extends JpaRepository<ServiceInfo, String> {
     ServiceInfo findDistinctFirstByVersion();
 }
