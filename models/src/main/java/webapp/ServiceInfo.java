c4e08aef13c7f03a6eb52efe97fe20e777fe12f6
Dilschat <dilschat@yandex.ru>
2019-05-28 12:24:09 +0500

    required constructors added to serviceInfo

--- models/src/main/java/webapp/ServiceInfo.java
+++ models/src/main/java/webapp/ServiceInfo.java
@@ -1,7 +1,5 @@
 package webapp;
 
-
-
 import org.springframework.lang.NonNull;
 
 import javax.persistence.Entity;
@@ -13,11 +11,20 @@ import javax.persistence.Table;
 @Table(name = "service_info")
 public class ServiceInfo {
     @Id
+    @NonNull
     private  String version;
-    private  String description;
-    private  String title;
-    private  String contact;
-    private  String licence;
+    @NonNull private  String description;
+    @NonNull private  String title;
+    @NonNull private  String contact;
+    @NonNull private  String licence;
 
     protected ServiceInfo() {}
+
+    public ServiceInfo(String version, String description, String title, String contact, String licence){
+        this.version = version;
+        this.description = description;
+        this.title = title;
+        this.contact = contact;
+        this.licence = licence;
+    }
 }
