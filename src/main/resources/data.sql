d5d8012fe874c021b5e806ce4c245e5eb7893a41
Dilschat <dilschat@yandex.ru>
2019-05-31 14:58:25 +0500

    fixed typo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,3 @@
-INSERT INTO service_info(version, description, title, contact, license) VALUES
+INSERT INTO service_info(version, description, title, contact, licence) VALUES
 ('version', 'description', 'title', 'contact', 'license')
+-- TODO solve problem with finding data sql by spring jpa
