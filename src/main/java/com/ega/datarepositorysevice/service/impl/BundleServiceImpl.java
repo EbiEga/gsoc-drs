ac8af8e9e016bcba78fe2ac76b151abc654e3058
Dilschat <dilschat@yandex.ru>
2019-06-07 16:17:26 +0500

    access method and bundle services implemented

---
+++ src/main/java/com/ega/datarepositorysevice/service/impl/BundleServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ega.datarepositorysevice.service.impl;
+
+import com.ega.datarepositorysevice.model.Bundle;
+import com.ega.datarepositorysevice.model.Object;
+import com.ega.datarepositorysevice.repository.BundleRepository;
+import com.ega.datarepositorysevice.service.BundleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+public class BundleServiceImpl implements BundleService {
+
+    @Autowired
+    BundleRepository bundleRepository;
+
+    @Override
+    public Mono<Bundle> getBundletById(String id) {
+        Optional<Bundle> bundleOpt =  bundleRepository.findById(id);
+        return bundleOpt.map(Mono::just).orElseGet(Mono::empty);
+    }
+}
