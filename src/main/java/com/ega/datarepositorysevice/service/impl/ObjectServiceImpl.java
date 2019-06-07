c7ea171d7642e849a8692f5feffd846581f4835d
Dilschat <dilschat@yandex.ru>
2019-06-07 16:15:04 +0500

    object service implemented

---
+++ src/main/java/com/ega/datarepositorysevice/service/impl/ObjectServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ega.datarepositorysevice.service.impl;
+
+import com.ega.datarepositorysevice.model.Object;
+import com.ega.datarepositorysevice.repository.ObjectRepository;
+import com.ega.datarepositorysevice.service.ObjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
+
+import java.util.Optional;
+
+public class ObjectServiceImpl implements ObjectService {
+
+    @Autowired
+    private ObjectRepository objectRepository;
+
+    @Override
+    public Mono<Object> getObjectById(String id) {
+        Optional<Object> objectOpt =  objectRepository.findById(id);
+        return objectOpt.map(Mono::just).orElseGet(Mono::empty);
+    }
+}
