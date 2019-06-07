252f683d8ca6e7915990b61ea7805a9f6e7fd8c8
Dilschat <dilschat@yandex.ru>
2019-06-07 12:45:38 +0500

    implemented test

---
+++ src/test/java/com/ega/datarepositorysevice/repository/BundleRepositoryTest.java
@@ -0,0 +1,44 @@
+package com.ega.datarepositorysevice.repository;
+
+
+import com.ega.datarepositorysevice.model.Bundle;
+import com.ega.datarepositorysevice.model.BundleObject;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.TimeZone;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class BundleRepositoryTest {
+
+    @Autowired
+    BundleRepository bundleRepository;
+
+    @Test
+    public void getBundleByIdTest() throws ParseException {
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        df.setTimeZone(TimeZone.getTimeZone("Europe/London"));
+        Bundle bundle = new Bundle("string","string",23,df.parse("2019-06-02T14:04:49.123Z"),
+                df.parse("2019-06-02T14:04:49.123Z"), "string", null,"string",
+                Arrays.asList("string"), Arrays.asList(new BundleObject("string", "string", "object",null, null)) );
+
+        bundleRepository.saveAndFlush(bundle);
+        Bundle responseBundle = bundleRepository.findById("string").get();
+        Assert.assertEquals(bundle.getId(), responseBundle.getId());
+        Assert.assertEquals(bundle.getName(), responseBundle.getName());
+        Assert.assertEquals(bundle.getSize(), responseBundle.getSize());
+
+
+
+    }
+}
