dbd0e19e3945215d1f1a6817dea0eb93bc66f701
Dilschat <dilschat@yandex.ru>
2019-06-15 00:35:37 +0500

    autowiring moved to constructor

--- src/main/java/com/ega/datarepositorysevice/service/impl/AccessMethodsServiceImpl.java
+++ src/main/java/com/ega/datarepositorysevice/service/impl/AccessMethodsServiceImpl.java
@@ -4,12 +4,12 @@ import com.ega.datarepositorysevice.model.AccessMethods;
 import com.ega.datarepositorysevice.repository.AccessMethodsRepository;
 import com.ega.datarepositorysevice.service.AccessMethodsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
-@Component
+@Service
 public class AccessMethodsServiceImpl implements AccessMethodsService {
 
     private final AccessMethodsRepository accessMethodsRepository;
