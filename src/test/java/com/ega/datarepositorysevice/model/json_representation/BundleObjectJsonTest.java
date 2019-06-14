2dd33d7b556c15a501eb97b4fd62ab13624f2dec
Dilschat <dilschat@yandex.ru>
2019-06-14 16:11:44 +0500

    bundleObject deserialization test finished

--- src/test/java/com/ega/datarepositorysevice/model/json_representation/BundleObjectJsonTest.java
+++ src/test/java/com/ega/datarepositorysevice/model/json_representation/BundleObjectJsonTest.java
@@ -3,6 +3,7 @@ package com.ega.datarepositorysevice.model.json_representation;
 
 import com.ega.datarepositorysevice.model.AccessMethods;
 import com.ega.datarepositorysevice.model.BundleObject;
+import com.ega.datarepositorysevice.model.Checksum;
 import com.ega.datarepositorysevice.model.Object;
 import com.ega.datarepositorysevice.model.enums.BundleObjectType;
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.nio.file.Files;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,5 +36,10 @@ public class BundleObjectJsonTest {
 
     @Test
     public void testDeserialize() throws Exception {
+        File file = ResourceUtils.getFile("classpath:model/bundle_object/bundle_object_valid.json");
+        BundleObject bundleObject = new BundleObject(Long.parseLong("1"), "name", BundleObjectType.OBJECT,null, null) ;
+
+        BundleObject parsedBundleObject = json.parseObject(new String(Files.readAllBytes(file.toPath())));
+        assertThat(parsedBundleObject).isEqualTo(bundleObject);
     }
 }
