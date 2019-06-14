592749196e1fcdca8e2ba9ba412c28115e478479
Dilschat <dilschat@yandex.ru>
2019-06-14 19:39:19 +0500

    all handlers implemented

--- src/main/java/com/ega/datarepositorysevice/controller/handler/AccessMethodHandler.java
+++ src/main/java/com/ega/datarepositorysevice/controller/handler/AccessMethodHandler.java
@@ -1,10 +1,9 @@
 package com.ega.datarepositorysevice.controller.handler;
 
+import com.ega.datarepositorysevice.model.AccessMethods;
 import com.ega.datarepositorysevice.model.Object;
-import com.ega.datarepositorysevice.service.ObjectService;
-import org.jboss.logging.Logger;
+import com.ega.datarepositorysevice.service.AccessMethodsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -12,21 +11,18 @@ import reactor.core.publisher.Mono;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
-@Service
-public class ObjectHandler {
-    final static Logger logger = Logger.getLogger(ObjectHandler.class);
-
-    private ObjectService objectService;
+public class AccessMethodHandler {
+    private AccessMethodsService accessMethodsService;
 
     @Autowired
-    public ObjectHandler(ObjectService objectService) {
-        this.objectService = objectService;
+    public AccessMethodHandler(AccessMethodsService objectService) {
+        this.accessMethodsService = objectService;
     }
 
     public Mono<ServerResponse> getObject(ServerRequest request){
-        String id = request.pathVariable("object_id");
+        String id = request.pathVariable("access_id");
         Mono<ServerResponse> notFound = ServerResponse.notFound().build();
-        Mono<Object> objectMono = objectService.getObjectById(Long.parseLong(id));
+        Mono<AccessMethods> objectMono = accessMethodsService.getAccessMethodsById(Long.parseLong(id));
 
         return objectMono.flatMap(user -> ServerResponse.ok()
                 .contentType(APPLICATION_JSON)
