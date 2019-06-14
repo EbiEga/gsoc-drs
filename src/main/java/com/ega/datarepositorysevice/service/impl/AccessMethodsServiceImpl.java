f201fb52a1c0fff65e6794d4d8dc58dcf5c5dc9f
Dilschat <dilschat@yandex.ru>
2019-06-15 00:35:37 +0500

    autowiring moved to constructor

--- src/main/java/com/ega/datarepositorysevice/service/impl/AccessMethodsServiceImpl.java
+++ src/main/java/com/ega/datarepositorysevice/service/impl/AccessMethodsServiceImpl.java
@@ -4,14 +4,20 @@ import com.ega.datarepositorysevice.model.AccessMethods;
 import com.ega.datarepositorysevice.repository.AccessMethodsRepository;
 import com.ega.datarepositorysevice.service.AccessMethodsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
+@Service
 public class AccessMethodsServiceImpl implements AccessMethodsService {
 
+    private final AccessMethodsRepository accessMethodsRepository;
+
     @Autowired
-    AccessMethodsRepository accessMethodsRepository;
+    public AccessMethodsServiceImpl(AccessMethodsRepository accessMethodsRepository) {
+        this.accessMethodsRepository = accessMethodsRepository;
+    }
 
     @Override
     public Mono<AccessMethods> getAccessMethodsById(Long id) {
--- src/main/java/com/ega/datarepositorysevice/service/impl/ObjectServiceImpl.java
+++ src/main/java/com/ega/datarepositorysevice/service/impl/ObjectServiceImpl.java
@@ -4,14 +4,20 @@ import com.ega.datarepositorysevice.model.Object;
 import com.ega.datarepositorysevice.repository.ObjectRepository;
 import com.ega.datarepositorysevice.service.ObjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
+@Service
 public class ObjectServiceImpl implements ObjectService {
 
+    private final ObjectRepository objectRepository;
+
     @Autowired
-    private ObjectRepository objectRepository;
+    public ObjectServiceImpl(ObjectRepository objectRepository) {
+        this.objectRepository = objectRepository;
+    }
 
     @Override
     public Mono<Object> getObjectById(Long id) {
