06151537f3fc7ae2dcdeb84268c9eee9e3a68c78
Dilschat <dilschat@yandex.ru>
2019-06-14 19:03:49 +0500

    null tests implemented for repositories

--- src/test/java/com/ega/datarepositorysevice/repository/AccessMethodRepositoryTest.java
+++ src/test/java/com/ega/datarepositorysevice/repository/AccessMethodRepositoryTest.java
@@ -1,46 +1,56 @@
-//package com.ega.datarepositorysevice.repository;
-//
-//import com.ega.datarepositorysevice.model.AccessMethods;
-//import com.ega.datarepositorysevice.model.AccessURL;
-//import com.ega.datarepositorysevice.model.Bundle;
-//import com.ega.datarepositorysevice.model.enums.AccessMethodType;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.text.ParseException;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDateTime;
-//import java.time.OffsetDateTime;
-//import java.time.ZoneOffset;
-//import java.util.Arrays;
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.TimeZone;
-//
-//@RunWith(SpringRunner.class)
-//@DataJpaTest
-//@AutoConfigureTestDatabase
-//public class AccessMethodRepositoryTest {
-//    @Autowired
-//    AccessMethodsRepository accessMethodsRepository;
-//
-//    @Test
-//    public void getBundleByIdTest() throws ParseException {
-//        LocalDateTime testDateTime = LocalDateTime.of(2018, 12, 12, 12, 12, 12, 121200000);
-//        OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
-//        Map<String, String> map = new HashMap<>();
-//        map.put("Authorization", "Basic Z2E0Z2g6ZHJz");
-//        AccessURL accessURL = new AccessURL("http//www.string.com",map);
-//        AccessMethods accessMethods = new AccessMethods(Long.parseLong("1"),AccessMethodType.S3,"region",null, null);
-//        accessMethodsRepository.save(accessMethods);
-//        AccessMethods responseAccessMethods = accessMethodsRepository.findById("access_id").get();
-//        Assert.assertEquals("1", responseAccessMethods.getAccessId());
-//        Assert.assertEquals("region", responseAccessMethods.getRegion());
-//        Assert.assertEquals("s3", responseAccessMethods.getType());
-//    }
-//}
+package com.ega.datarepositorysevice.repository;
+
+import com.ega.datarepositorysevice.model.AccessMethods;
+import com.ega.datarepositorysevice.model.AccessURL;
+import com.ega.datarepositorysevice.model.Bundle;
+import com.ega.datarepositorysevice.model.enums.AccessMethodType;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class AccessMethodRepositoryTest {
+    @Autowired
+    AccessMethodsRepository accessMethodsRepository;
+
+    @Test
+    public void getBundleByIdTest() throws ParseException {
+        LocalDateTime testDateTime = LocalDateTime.of(2018, 12, 12, 12, 12, 12, 121200000);
+        OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
+        Map<String, String> map = new HashMap<>();
+        map.put("Authorization", "Basic Z2E0Z2g6ZHJz");
+        AccessURL accessURL = new AccessURL("http//www.string.com",map);
+        AccessMethods accessMethods = new AccessMethods(1L,AccessMethodType.S3,"region",null, null);
+        accessMethods = accessMethodsRepository.save(accessMethods);
+        AccessMethods responseAccessMethods = accessMethodsRepository.findById(accessMethods.getAccessId()).get();
+        Assert.assertEquals(accessMethods.getAccessId(), responseAccessMethods.getAccessId());
+        Assert.assertEquals("region", responseAccessMethods.getRegion());
+        Assert.assertEquals("s3", responseAccessMethods.getType());
+    }
+
+    @Test
+    public void getBundleByIdNullTest() throws ParseException {
+        LocalDateTime testDateTime = LocalDateTime.of(2018, 12, 12, 12, 12, 12, 121200000);
+        OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
+        Map<String, String> map = new HashMap<>();
+        map.put("Authorization", "Basic Z2E0Z2g6ZHJz");
+        AccessURL accessURL = new AccessURL("http//www.string.com",map);
+        AccessMethods accessMethods = new AccessMethods(Long.parseLong("5"),AccessMethodType.S3,"region",null, null);
+        accessMethodsRepository.save(accessMethods);
+        Optional<AccessMethods> responseAccessMethods = accessMethodsRepository.findById(Long.parseLong("3"));
+        Assert.assertFalse(responseAccessMethods.isPresent());
+    }
+}
--- src/test/java/com/ega/datarepositorysevice/repository/ObjectRepositoryTest.java
+++ src/test/java/com/ega/datarepositorysevice/repository/ObjectRepositoryTest.java
@@ -1,41 +1,53 @@
-//package com.ega.datarepositorysevice.repository;
-//
-//import com.ega.datarepositorysevice.model.Bundle;
-//import com.ega.datarepositorysevice.model.Object;
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.text.ParseException;
-//import java.text.SimpleDateFormat;
-//import java.time.LocalDateTime;
-//import java.time.OffsetDateTime;
-//import java.time.ZoneOffset;
-//import java.util.Arrays;
-//import java.util.TimeZone;
-//
-//@RunWith(SpringRunner.class)
-//@DataJpaTest
-//@AutoConfigureTestDatabase
-//public class ObjectRepositoryTest {
-//
-//    @Autowired
-//    ObjectRepository objectRepository;
-//
-//    @Test
-//    public void getBundleByIdTest() throws ParseException {
-//        LocalDateTime testDateTime = LocalDateTime.of(2018, 12, 12, 12, 12, 12, 121200000);
-//        OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
-//        Object object = new Object(Long.parseLong("1"), "string", 0, null,  date, "string", "application/json",
-//                null, null, "string", null);
-//        objectRepository.save(object);
-//        Object responseObject = objectRepository.findById("id").get();
-//        Assert.assertEquals("1", responseObject.getId());
-//        Assert.assertEquals("string", responseObject.getName());
-//        Assert.assertEquals(0, responseObject.getSize());
-//    }
-//}
+package com.ega.datarepositorysevice.repository;
+
+import com.ega.datarepositorysevice.model.Bundle;
+import com.ega.datarepositorysevice.model.Object;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.TimeZone;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase
+public class ObjectRepositoryTest {
+
+    @Autowired
+    ObjectRepository objectRepository;
+
+    @Test
+    public void getBundleByIdTest() throws ParseException {
+        LocalDateTime testDateTime = LocalDateTime.of(2018, 12, 12, 12, 12, 12, 121200000);
+        OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
+        Object object = new Object(1L, "string", 0, null,  date, "string", "application/json",
+                null, null, "string", null);
+        object = objectRepository.save(object);
+        Object responseObject = objectRepository.findById(object.getId()).get();
+        Assert.assertEquals(object.getId(), responseObject.getId());
+        Assert.assertEquals("string", responseObject.getName());
+        Assert.assertEquals(0, responseObject.getSize());
+    }
+
+    @Test
+    public void getBundleByIdNullTest() throws ParseException {
+        LocalDateTime testDateTime = LocalDateTime.of(2018, 12, 12, 12, 12, 12, 121200000);
+        OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
+        Object object = new Object(Long.parseLong("5"), "string", 0, null,  date, "string", "application/json",
+                null, null, "string", null);
+        objectRepository.save(object);
+        Optional<Object> responseObject = objectRepository.findById(Long.parseLong("3"));
+        Assert.assertTrue(!responseObject.isPresent());
+    }
+}
