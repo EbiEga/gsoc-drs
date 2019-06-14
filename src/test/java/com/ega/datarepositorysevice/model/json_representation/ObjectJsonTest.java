67a2b8a13a69054b4852f4cdb334fc46ee82c09a
Dilschat <dilschat@yandex.ru>
2019-06-14 15:50:26 +0500

    deserialization test implemented

--- src/test/java/com/ega/datarepositorysevice/model/json_representation/ObjectJsonTest.java
+++ src/test/java/com/ega/datarepositorysevice/model/json_representation/ObjectJsonTest.java
@@ -6,6 +6,7 @@ import com.ega.datarepositorysevice.model.Checksum;
 import com.ega.datarepositorysevice.model.Object;
 import com.ega.datarepositorysevice.model.ServiceInfo;
 import com.google.api.client.util.DateTime;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.ResourceUtils;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
@@ -41,11 +43,19 @@ public class ObjectJsonTest {
         OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
         Object object = new Object(Long.parseLong("1"), "string", 0, date,  date, "string", "application/json",
                 null, null, "string", null);
-        System.out.println(json.write(object));
         assertThat(json.write(object)).isEqualToJson(file);
     }
 
     @Test
     public void testDeserialize() throws Exception {
+        File file = ResourceUtils.getFile("classpath:model/object/object_null.json");
+        LocalDateTime testDateTime = LocalDateTime.of(2018,12,12,12,12,12,121200000);
+        OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
+        Object object = new Object(Long.parseLong("1"), "string", 0, date,  date, "string", "application/json",
+                null, null, "string", null);
+
+        Object parsedObject = json.parseObject(new String(Files.readAllBytes(file.toPath())));
+        Assert.assertEquals(object, parsedObject);
+
     }
 }
