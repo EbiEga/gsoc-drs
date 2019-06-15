fbd2fb8a65088c544873d7d6bf7d9d4eda8c930f
Dilschat <dilschat@yandex.ru>
2019-06-16 00:43:02 +0500

    BundleService tests implemented

---
+++ src/test/java/com/ega/datarepositorysevice/service/BundleServiceTest.java
@@ -0,0 +1,63 @@
+package com.ega.datarepositorysevice.service;
+
+
+import com.ega.datarepositorysevice.model.AccessMethods;
+import com.ega.datarepositorysevice.model.Bundle;
+import com.ega.datarepositorysevice.repository.BundleRepository;
+import com.ega.datarepositorysevice.service.impl.BundleServiceImpl;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class BundleServiceTest {
+
+    private BundleService bundleService;
+    private Bundle bundleTestObject;
+
+
+    @Before
+    public void prepareDatabase(){
+        LocalDateTime testDateTime = LocalDateTime.of(2018, 12, 12, 12, 12, 12, 121200000);
+        OffsetDateTime date = OffsetDateTime.of(testDateTime, ZoneOffset.ofHours(2));
+        bundleTestObject = new Bundle(1L, "string", 23, date,
+                date, "string", null, "string",
+                Arrays.asList("string"), Arrays.asList());
+
+        BundleRepository bundleRepository = mock(BundleRepository.class);
+        when(bundleRepository.findById(1L)).thenReturn(Optional.of(bundleTestObject));
+        when(bundleRepository.findById(2L)).thenReturn(Optional.empty());
+
+        bundleService = new BundleServiceImpl(bundleRepository);
+
+    }
+
+    @Test
+    public void testExistingValue(){
+        Mono<Bundle> bundleMono = bundleService.getBundletById(1L);
+        Optional<Bundle> bundleOptional = bundleMono.blockOptional();
+
+        Assert.assertTrue(bundleOptional.isPresent());
+        Assert.assertEquals(bundleTestObject, bundleOptional.get());
+
+
+    }
+
+    @Test
+    public void testEmptyValue(){
+        Mono<Bundle> bundleMono = bundleService.getBundletById(2L);
+        Optional<Bundle> bundleOptional = bundleMono.blockOptional();
+
+        Assert.assertFalse(bundleOptional.isPresent());
+    }
+}
