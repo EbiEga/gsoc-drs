165486efd155f6ef205f9e362ea4050e42056f90
Dilschat <dilschat@yandex.ru>
2019-06-23 18:16:22 +0500

    test enviroment fixed

--- src/test/java/com/ega/datarepositorysevice/service/ObjectServiceIntegrationTest.java
+++ src/test/java/com/ega/datarepositorysevice/service/ObjectServiceIntegrationTest.java
@@ -13,6 +13,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Mono;
