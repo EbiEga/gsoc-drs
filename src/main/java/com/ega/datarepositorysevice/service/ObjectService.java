69accd510fe35244a035b0a667a3c00a25a870d0
Dilschat <dilschat@yandex.ru>
2019-06-23 18:24:54 +0500

    fixed bean annotation

--- src/main/java/com/ega/datarepositorysevice/service/ObjectService.java
+++ src/main/java/com/ega/datarepositorysevice/service/ObjectService.java
@@ -4,7 +4,6 @@ import com.ega.datarepositorysevice.model.Object;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
-@Service
 public interface ObjectService {
     Mono<Object> getObjectById(Long id);
 }
