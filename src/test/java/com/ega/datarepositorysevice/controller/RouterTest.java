7c38df8d247d81914c9952be6ef0a1f78b2de92c
Dilschat <dilschat@yandex.ru>
2019-06-23 23:26:50 +0500

    router testing finished

---
+++ src/test/java/com/ega/datarepositorysevice/controller/RouterTest.java
@@ -0,0 +1,122 @@
+package com.ega.datarepositorysevice.controller;
+
+import com.ega.datarepositorysevice.model.AccessMethods;
+import com.ega.datarepositorysevice.model.Bundle;
+import com.ega.datarepositorysevice.model.Object;
+import com.ega.datarepositorysevice.repository.AccessMethodsRepository;
+import com.ega.datarepositorysevice.repository.BundleRepository;
+import com.ega.datarepositorysevice.repository.ObjectRepository;
+import com.ega.datarepositorysevice.utils.TestObjectCreator;
+import com.sun.org.apache.xml.internal.utils.URI;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+
+
+@Import(Router.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@AutoConfigureTestDatabase
+public class RouterTest {
+
+    @Autowired
+    AccessMethodsRepository accessMethodsRepository;
+
+    @Autowired
+    ObjectRepository objectRepository;
+
+    @Autowired
+    BundleRepository bundleRepository;
+
+    @Autowired
+    Router router;
+
+    private AccessMethods accessMethods;
+    private Object object;
+    private Bundle bundle;
+
+    private WebTestClient webTestClient;
+
+    @Before
+    public void prepareEnvironment() throws URI.MalformedURIException {
+        webTestClient = WebTestClient.bindToRouterFunction(router.route()).build();
+
+
+        accessMethods = TestObjectCreator.getAccessMethods();
+        object = TestObjectCreator.getObject();
+        bundle = TestObjectCreator.getBundle();
+
+        accessMethods = accessMethodsRepository.save(accessMethods);
+        object = objectRepository.save(object);
+        bundle = bundleRepository.save(bundle);
+    }
+
+
+    @Test
+    public void testAccessMethodsPathOk() throws IOException {
+        webTestClient.get()
+                .uri(String.format("/objects/%d/access/%d",object.getId(),accessMethods.getAccessId()))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(AccessMethods.class).isEqualTo(accessMethods);
+    }
+
+    @Test
+    public void testAccessMethodsPathNotFound(){
+        webTestClient.get()
+                .uri(String.format("/objects/%s/access/%s","0","0"))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    public void testObjectPathOk() throws IOException {
+
+        webTestClient.get()
+                .uri(String.format("/objects/%s",object.getId()))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Object.class).isEqualTo(object);
+    }
+
+    @Test
+    public void testObjectPathNotFound(){
+        webTestClient.get()
+                .uri(String.format("/objects/%d",0))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound();
+    }    @Test
+    public void testBundlePathOk() throws IOException {
+        webTestClient.get()
+                .uri(String.format("/bundles/%d",bundle.getId()))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(Bundle.class).isEqualTo(bundle);
+    }    @Test
+    public void testBundlePathNotFound(){
+        webTestClient.get()
+                .uri(String.format("/bundles/%d",0))
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+}
