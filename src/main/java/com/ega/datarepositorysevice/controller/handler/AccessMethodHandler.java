4c58dc60c64f32686744277aa0608e1cc9a5f4ca
Dilschat <dilschat@yandex.ru>
2019-06-25 03:11:01 +0500

    handlers reformatted and error handler added

--- src/main/java/com/ega/datarepositorysevice/controller/handler/AccessMethodHandler.java
+++ src/main/java/com/ega/datarepositorysevice/controller/handler/AccessMethodHandler.java
@@ -1,14 +1,19 @@
 package com.ega.datarepositorysevice.controller.handler;
 
+import com.ega.datarepositorysevice.controller.HandlerUtils;
 import com.ega.datarepositorysevice.model.AccessMethods;
+import com.ega.datarepositorysevice.model.Error;
 import com.ega.datarepositorysevice.service.AccessMethodsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import static com.ega.datarepositorysevice.controller.HandlerUtils.ACCESS_METHODS_PATH_VARIABLE;
+import static com.ega.datarepositorysevice.controller.HandlerUtils.retrievePathVariable;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 
 @Component
@@ -21,13 +26,15 @@ public class AccessMethodHandler {
     }
 
     public Mono<ServerResponse> getAccess(ServerRequest request) {
-        String id = request.pathVariable("access_id");
-        Mono<ServerResponse> notFound = ServerResponse.notFound().build();
-        Mono<AccessMethods> objectMono = accessMethodsService.getAccessMethodsById(Long.parseLong(id));
+        try {
+            Error notFoundError = new Error("The requested AccessMethod wasn't found", HttpStatus.NOT_FOUND);
+            Mono<AccessMethods> accessMethodMono = accessMethodsService
+                    .getAccessMethodsById(retrievePathVariable(request,ACCESS_METHODS_PATH_VARIABLE));
+            return HandlerUtils.returnOkResponse(accessMethodMono, notFoundError);
+        } catch (IllegalArgumentException e){
+            return HandlerUtils.returnBadRequest(e);
+        }
 
-        return objectMono.flatMap(user -> ServerResponse.ok()
-                .contentType(APPLICATION_JSON)
-                .body(BodyInserters.fromObject(user)))
-                .switchIfEmpty(notFound);
+
     }
 }
