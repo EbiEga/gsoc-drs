1f2cc8db845e4908ba87091d007ce7648d77ef7a
Dilschat <dilschat@yandex.ru>
2019-06-25 23:49:09 +0500

    static router implemented

--- src/main/java/com/ega/datarepositorysevice/controller/Router.java
+++ src/main/java/com/ega/datarepositorysevice/controller/Router.java
@@ -4,8 +4,11 @@ import com.ega.datarepositorysevice.controller.handler.AccessMethodHandler;
 import com.ega.datarepositorysevice.controller.handler.BundleHandler;
 import com.ega.datarepositorysevice.controller.handler.ObjectHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -13,6 +16,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Configuration
 public class Router {
@@ -25,6 +29,8 @@ public class Router {
 
     private final
     AccessMethodHandler accessMethodHandler;
+
+    @Value("classpath:/static/service-info.json") Resource serviceInfo;
 
     @Autowired
     public Router(ObjectHandler objectHandler, BundleHandler bundleHandler, AccessMethodHandler accessMethodHandler) {
@@ -39,7 +45,19 @@ public class Router {
         return RouterFunctions
                 .route(GET("/objects/{object_id}").and(accept(APPLICATION_JSON)), objectHandler::getObject)
                 .andRoute(GET("/bundles/{bundle_id}").and(accept(APPLICATION_JSON)), bundleHandler::getBundle)
-                .andRoute(GET("/objects/{object_id}/access/{access_id}").and(accept(APPLICATION_JSON)),accessMethodHandler::getAccess);
+                .andRoute(GET("/objects/{object_id}/access/{access_id}").and(accept(APPLICATION_JSON)),accessMethodHandler::getAccess)
+                .and(staticRouter());
+
     }
+
+
+    public RouterFunction<ServerResponse> staticRouter() {
+        return RouterFunctions.route(GET("/service-info"), request ->
+                ok().contentType(MediaType.APPLICATION_JSON).syncBody(serviceInfo)
+
+        );
+    }
+
+
 
 }
