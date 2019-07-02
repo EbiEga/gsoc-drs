46f104b7ec4daa37cfb4a710a0a8674f4bf1afc4
Dilschat <dilschat@yandex.ru>
2019-07-03 02:55:46 +0500

    contextLoads test fixed using conditional run

--- src/test/java/com/ega/datarepositorysevice/DataRepositorySeviceApplicationTests.java
+++ src/test/java/com/ega/datarepositorysevice/DataRepositorySeviceApplicationTests.java
@@ -1,14 +1,22 @@
 package com.ega.datarepositorysevice;
 
+import com.ega.datarepositorysevice.utils.AssumingConnection;
+import com.ega.datarepositorysevice.utils.ConnectionChecker;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class DataRepositorySeviceApplicationTests {
+
+	@ClassRule
+	public static AssumingConnection assumingConnection =
+			new AssumingConnection(new ConnectionChecker());
 
 	@Test
 	public void contextLoads() {
