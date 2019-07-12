21f0f4329f68e916b5ac88501aa345116d4f186f
Dilschat <dilschat@yandex.ru>
2019-07-12 20:09:57 +0500

    auth tests added

--- src/test/java/com/ega/datarepositorysevice/auth/RouterAuthTest.java
+++ src/test/java/com/ega/datarepositorysevice/auth/RouterAuthTest.java
@@ -1,5 +1,6 @@
 package com.ega.datarepositorysevice.auth;
 
+import com.ega.datarepositorysevice.DataRepositoryServiceApplication;
 import com.ega.datarepositorysevice.WebSecurityConfig;
 import com.ega.datarepositorysevice.controller.Router;
 import com.ega.datarepositorysevice.controller.handler.AccessMethodHandler;
@@ -12,6 +13,9 @@ import com.ega.datarepositorysevice.model.Object;
 import com.ega.datarepositorysevice.repository.AccessMethodsRepository;
 import com.ega.datarepositorysevice.repository.BundleRepository;
 import com.ega.datarepositorysevice.repository.ObjectRepository;
+import com.ega.datarepositorysevice.service.AccessMethodsService;
+import com.ega.datarepositorysevice.service.BundleService;
+import com.ega.datarepositorysevice.service.ObjectService;
 import com.ega.datarepositorysevice.utils.TestObjectCreator;
 import com.google.api.client.util.Base64;
 import com.sun.org.apache.xml.internal.utils.URI;
@@ -22,19 +26,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebFlux;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.server.ServerRequest;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -42,24 +53,25 @@ import java.util.Objects;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@Import({Router.class,WebSecurityConfig.class})
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @AutoConfigureTestDatabase
-@ActiveProfiles("dev")
+@ContextConfiguration(classes = {Router.class, AccessMethodHandler.class, ObjectHandler.class, BundleHandler.class, WebSecurityConfig.class})
+@ActiveProfiles({"dev","prod"})
+@WebFluxTest
 public class RouterAuthTest {
-
     @Autowired
-    AccessMethodsRepository accessMethodsRepository;
+    private ApplicationContext context;
 
-    @Autowired
-    ObjectRepository objectRepository;
+    @MockBean
+    ObjectService objectService;
+    @MockBean
+    BundleService bundleService;
+    @MockBean
+    AccessMethodsService accessMethodsService;
 
-    @Autowired
-    BundleRepository bundleRepository;
 
-    @Autowired
-    Router router;
+//    @Autowired
+//    Router router;
 
     @Value("${security.oauth2.client.client-id}")
     String clientId;
@@ -71,19 +83,23 @@ public class RouterAuthTest {
     private Object object;
     private Bundle bundle;
 
-    private WebTestClient webTestClient;
+//    @Autowired
+     private WebTestClient webTestClient;
 
     @Before
     public void prepareEnvironment() throws URI.MalformedURIException {
-        webTestClient = WebTestClient.bindToRouterFunction(router.route()).build();
+        webTestClient = WebTestClient.bindToApplicationContext(context).build();
+        when(objectService.getObjectById(1L)).thenReturn(Mono.empty());
+        when(accessMethodsService.getAccessMethodsById(1L)).thenReturn(Mono.empty());
+        when(bundleService.getBundleById(1L)).thenReturn(Mono.empty());
 
-        accessMethods = TestObjectCreator.getAccessMethods();
-        object = TestObjectCreator.getObject();
-        bundle = TestObjectCreator.getBundle();
-
-        accessMethods = accessMethodsRepository.save(accessMethods);
-        object = objectRepository.save(object);
-        bundle = bundleRepository.save(bundle);
+//        accessMethods = TestObjectCreator.getAccessMethods();
+//        object = TestObjectCreator.getObject();
+//        bundle = TestObjectCreator.getBundle();
+//
+//        accessMethods = accessMethodsRepository.save(accessMethods);
+//        object = objectRepository.save(object);
+//        bundle = bundleRepository.save(bundle);
 
 
 
@@ -94,19 +110,19 @@ public class RouterAuthTest {
     @Test
     public void testAccessMethodsPathOk() throws IOException {
         webTestClient.get()
-                .uri(String.format("/objects/%d/access/%d", object.getId(), accessMethods.getAccessId()))
-                .headers(h -> h.setBearerAuth(getToken("", "dN9yCSbQ")))
-                .accept(MediaType.APPLICATION_JSON)
+                .uri("/objects/1/access/1")
+                .headers(h -> h.setBearerAuth(getToken("amp-dev@ebi.ac.uk", "dN9yCSbQ")))
+               // .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus().isNotFound();
 
     }
 
     @Test
     public void testAccessMethodsPathUnauthorized() throws IOException {
         webTestClient.get()
-                .uri(String.format("/objects/%d/access/%d", object.getId(), accessMethods.getAccessId()))
-                .headers(h -> h.setBearerAuth(""))
+                .uri("/objects/1/access/1")
+                .headers(h -> h.setBearerAuth("r"))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isUnauthorized();
@@ -118,19 +134,19 @@ public class RouterAuthTest {
     public void testObjectPathOk() throws IOException {
 
         webTestClient.get()
-                .uri(String.format("/objects/%s", object.getId()))
-                .headers(h -> h.setBearerAuth(getToken("", "dN9yCSbQ")))
+                .uri("/objects/1")
+                .headers(h -> h.setBearerAuth(getToken("amp-dev@ebi.ac.uk", "dN9yCSbQ")))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus().isNotFound();
     }
 
     @Test
     public void testObjectPathUnauthorized() throws IOException {
 
         webTestClient.get()
-                .uri(String.format("/objects/%s", object.getId()))
-                .headers(h -> h.setBearerAuth(""))
+                .uri("/objects/1")
+                .headers(h -> h.setBearerAuth("r"))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isUnauthorized();
@@ -141,18 +157,18 @@ public class RouterAuthTest {
     @Test
     public void testBundlePathOk() throws IOException {
         webTestClient.get()
-                .uri(String.format("/bundles/%d", bundle.getId()))
-                .headers(h -> h.setBearerAuth(getToken("", "dN9yCSbQ")))
+                .uri("/bundles/1")
+                .headers(h -> h.setBearerAuth(getToken("amp-dev@ebi.ac.uk", "dN9yCSbQ")))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus().isNotFound();
     }
 
     @Test
     public void testBundlePathUnauthorized() throws IOException {
         webTestClient.get()
-                .uri(String.format("/bundles/%d", bundle.getId()))
-                .headers(h -> h.setBearerAuth(""))
+                .uri("/bundles/1")
+                .headers(h -> h.setBearerAuth("r"))
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
                 .expectStatus().isUnauthorized();
@@ -166,7 +182,17 @@ public class RouterAuthTest {
                 .uri(String.format("/bundles/%s", "qeqw=2131"))
                 .accept()
                 .exchange()
-                .expectStatus().isBadRequest();
+                .expectStatus().isUnauthorized();
+    }
+
+    @Test
+    public void testServiceInfoOk() throws IOException {
+
+        webTestClient.get()
+                .uri("/service-info")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
     }
 
 
