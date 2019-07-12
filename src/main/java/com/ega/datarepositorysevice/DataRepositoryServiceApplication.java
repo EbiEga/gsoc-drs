0bdf6b24bd751ccb77c0aaf77533a67e71a8a991
Dilschat <dilschat@yandex.ru>
2019-07-12 20:09:14 +0500

    required annotations added(resource server enabled)

--- src/main/java/com/ega/datarepositorysevice/DataRepositoryServiceApplication.java
+++ src/main/java/com/ega/datarepositorysevice/DataRepositoryServiceApplication.java
@@ -2,8 +2,13 @@ package com.ega.datarepositorysevice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.reactive.config.EnableWebFlux;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableWebFlux
+@EnableResourceServer
 public class DataRepositoryServiceApplication {
 
 	public static void main(String[] args) {
