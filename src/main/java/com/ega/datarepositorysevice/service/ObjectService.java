358d9ad6a44d5a09b399609b249766cc18c5ee4c
Dilschat <dilschat@yandex.ru>
2019-07-15 02:16:52 +0500

    ObjectService interface extended

--- src/main/java/com/ega/datarepositorysevice/service/ObjectService.java
+++ src/main/java/com/ega/datarepositorysevice/service/ObjectService.java
@@ -6,4 +6,10 @@ import reactor.core.publisher.Mono;
 
 public interface ObjectService {
     Mono<Object> getObjectById(Long id);
+
+    boolean deleteObjectById(Long id);
+
+    Mono<Object> saveObject(Mono<Object> objectMono);
+
+    Mono<Object> updateObject(Mono<Object> objectMono);
 }
