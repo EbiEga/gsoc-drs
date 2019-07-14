0e345626e5cbe1f2a4d6598ffebda0b5a367b132
Dilschat <dilschat@yandex.ru>
2019-07-14 19:07:57 +0500

    value pulling from properties file added

--- src/main/java/com/ega/datarepositorysevice/WebSecurityConfig.java
+++ src/main/java/com/ega/datarepositorysevice/WebSecurityConfig.java
@@ -2,22 +2,22 @@ package com.ega.datarepositorysevice;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoders;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-//@EnableWebFluxSecurity
-//@EnableReactiveMethodSecurity
-@EnableWebFluxSecurity
-@Profile({"dev","prod"})
-public class WebSecurityConfig{
 
-//    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-//    private String issuerUri;
+@Configuration
+@EnableWebFluxSecurity
+@Profile({"dev", "prod"})
+public class WebSecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
@@ -30,12 +30,11 @@ public class WebSecurityConfig{
                 .jwt();
         return http.build();
     }
-//
-@Bean
-public ReactiveJwtDecoder jwtDecoder() {
-    return ReactiveJwtDecoders.fromOidcIssuerLocation("https://ega.ebi.ac.uk:8443/ega-openid-connect-server/");
-}
 
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return ReactiveJwtDecoders.fromOidcIssuerLocation(issuerUri);
+    }
 
 
 }
