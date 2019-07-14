a6f45f74b07622b7e651e1d6f0feea22be4dd572
Dilschat <dilschat@yandex.ru>
2019-07-15 02:30:07 +0500

    Router function extended

--- src/main/java/com/ega/datarepositorysevice/controller/Router.java
+++ src/main/java/com/ega/datarepositorysevice/controller/Router.java
@@ -14,8 +14,9 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RequestPredicates.PUT;
 import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 
 @Configuration
@@ -30,8 +31,7 @@ public class Router {
     private final
     AccessMethodHandler accessMethodHandler;
 
-    @Value("classpath:/static/service-info.json")
-    private Resource serviceInfo;
+    @Value("classpath:/static/service-info.json") Resource serviceInfo;
 
     @Autowired
     public Router(ObjectHandler objectHandler, BundleHandler bundleHandler, AccessMethodHandler accessMethodHandler) {
@@ -42,22 +42,50 @@ public class Router {
 
 
     @Bean
-    public RouterFunction<ServerResponse> route() {
-        return RouterFunctions
-                .route(GET("/objects/{object_id}").and(accept(APPLICATION_JSON)), objectHandler::getObject)
-                .andRoute(GET("/bundles/{bundle_id}").and(accept(APPLICATION_JSON)), bundleHandler::getBundle)
-                .andRoute(GET("/objects/{object_id}/access/{access_id}").and(accept(APPLICATION_JSON)), accessMethodHandler::getAccess)
-                .and(staticRouter());
+    public RouterFunction<ServerResponse> route(){
+        return RouterFunctions.route()
+                .add(getRouter())
+                .add(postRouter())
+                .add(updateRouter())
+                .add(deleteRouter())
+                .add(staticRouter())
+                .build();
 
     }
 
+    private RouterFunction<ServerResponse> getRouter(){
+        return RouterFunctions
+                .route(GET("/objects/{object_id}").and(accept(APPLICATION_JSON)), objectHandler::getObject)
+                .andRoute(GET("/bundles/{bundle_id}").and(accept(APPLICATION_JSON)), bundleHandler::getBundle)
+                .andRoute(GET("/objects/{object_id}/access/{access_id}").and(accept(APPLICATION_JSON)),accessMethodHandler::getAccess);
+    }
 
-    public RouterFunction<ServerResponse> staticRouter() {
+    private RouterFunction<ServerResponse> postRouter(){
+        return RouterFunctions
+                .route(POST("/objects").and(accept(APPLICATION_JSON)), objectHandler::saveObject)
+                .andRoute(POST("/bundles").and(accept(APPLICATION_JSON)), bundleHandler::saveBundle)
+                .andRoute(POST("/objects/{object_id}/access").and(accept(APPLICATION_JSON)),accessMethodHandler::saveAccess);
+    }
+
+    private RouterFunction<ServerResponse> updateRouter(){
+        return RouterFunctions
+                .route(PUT("/objects/{object_id}").and(accept(APPLICATION_JSON)), objectHandler::updateObject)
+                .andRoute(PUT("/bundles/{bundle_id}").and(accept(APPLICATION_JSON)), bundleHandler::updateBundle)
+                .andRoute(PUT("/objects/{object_id}/access/{access_id}").and(accept(APPLICATION_JSON)),accessMethodHandler::updateAccess);
+    }
+
+    private RouterFunction<ServerResponse> deleteRouter(){
+        return RouterFunctions
+                .route(DELETE("/objects/{object_id}").and(accept(APPLICATION_JSON)), objectHandler::deleteObject)
+                .andRoute(DELETE("/bundles/{bundle_id}").and(accept(APPLICATION_JSON)), bundleHandler::deleteBundle)
+                .andRoute(DELETE("/objects/{object_id}/access/{access_id}").and(accept(APPLICATION_JSON)),accessMethodHandler::deleteAccess);
+    }
+
+    private RouterFunction<ServerResponse> staticRouter() {
         return RouterFunctions.route(GET("/service-info"), request ->
                 ok().contentType(MediaType.APPLICATION_JSON).syncBody(serviceInfo)
 
         );
     }
-
 
 }
