030041d16dd3b12c34239ce6035034eccb15b93d
Dilschat <dilschat@yandex.ru>
2019-07-18 03:57:48 +0500

    acess method service implemented

--- src/main/java/com/ega/datarepositorysevice/service/impl/AccessMethodsServiceImpl.java
+++ src/main/java/com/ega/datarepositorysevice/service/impl/AccessMethodsServiceImpl.java
@@ -1,8 +1,11 @@
 package com.ega.datarepositorysevice.service.impl;
 
 import com.ega.datarepositorysevice.model.AccessMethods;
+import com.ega.datarepositorysevice.model.Object;
 import com.ega.datarepositorysevice.repository.AccessMethodsRepository;
+import com.ega.datarepositorysevice.repository.ObjectRepository;
 import com.ega.datarepositorysevice.service.AccessMethodsService;
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -13,42 +16,66 @@ import java.util.Optional;
 public class AccessMethodsServiceImpl implements AccessMethodsService {
 
     private final AccessMethodsRepository accessMethodsRepository;
+    private final ObjectRepository objectRepository;
 
     @Autowired
-    public AccessMethodsServiceImpl(AccessMethodsRepository accessMethodsRepository) {
+    public AccessMethodsServiceImpl(AccessMethodsRepository accessMethodsRepository, ObjectRepository objectRepository) {
         this.accessMethodsRepository = accessMethodsRepository;
+        this.objectRepository = objectRepository;
     }
 
     @Override
-    public Mono<AccessMethods> getAccessMethodsById(Long id) {
+    public Mono<AccessMethods> getAccessMethodsById(Long objectId,Long id) {
+        try {
+            checkIsObjectExist(objectId);
+        }catch (IllegalArgumentException e){
+            return Mono.error(e);
+        }
         Optional<AccessMethods> accessMethodsOpt = accessMethodsRepository.findById(id);
         return accessMethodsOpt.map(Mono::just).orElseGet(Mono::empty);
     }
 
+
     @Override
-    public Mono<AccessMethods> saveAccessMethod(Mono<AccessMethods> accessMethods) {
-        return accessMethods.map(accessMethodsRepository::save);
+    public Mono<AccessMethods> saveAccessMethod(Long objectId, Mono<AccessMethods> accessMethods) {
+        return accessMethods.map(methods -> {
+            checkIsObjectExist(objectId);
+            Optional<Object> objectOptional = objectRepository.findById(objectId);
+            Object object = objectOptional.get();
+            methods.setObject(object);
+            return accessMethodsRepository.save(methods);
+        });
+
     }
 
     @Override
-    public boolean deleteById(Long id) {
+    public Mono<Boolean> deleteById(Long objectId, Long id)  {
         try {
+            checkIsObjectExist(objectId);
             accessMethodsRepository.deleteById(id);
-            return true;
+            return Mono.just(true);
         }catch (IllegalArgumentException e){
-            return false;
+            return Mono.error(e);
         }
     }
 
     @Override
-    public Mono<AccessMethods> updateAccessMethod(Mono<AccessMethods> accessMethodsMono) {
+    public Mono<AccessMethods> updateAccessMethod(Long objectId,Mono<AccessMethods> accessMethodsMono)  {
         return accessMethodsMono.map(accessMethods -> {
+            checkIsObjectExist(objectId);
             if(accessMethodsRepository.existsById(accessMethods.getAccessId())){
                 return accessMethodsRepository.save(accessMethods);
             }else {
                 throw new IllegalArgumentException("id of access method is not found");
             }
         });
+
+    }
+
+    private void checkIsObjectExist(Long objectId) {
+        if(!objectRepository.existsById(objectId)) {
+            throw new IllegalArgumentException("Object by given id not found");
+        }
     }
 
 
