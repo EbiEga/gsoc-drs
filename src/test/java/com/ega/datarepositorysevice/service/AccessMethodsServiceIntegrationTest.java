b84418b68f323d5125a17ddd26753a6375497e0a
Dilschat <dilschat@yandex.ru>
2019-07-20 03:25:00 +0500

    integration test fixed

--- src/test/java/com/ega/datarepositorysevice/service/AccessMethodsServiceIntegrationTest.java
+++ src/test/java/com/ega/datarepositorysevice/service/AccessMethodsServiceIntegrationTest.java
@@ -2,8 +2,11 @@ package com.ega.datarepositorysevice.service;
 
 import com.ega.datarepositorysevice.model.AccessMethods;
 import com.ega.datarepositorysevice.model.AccessURL;
+import com.ega.datarepositorysevice.model.Object;
 import com.ega.datarepositorysevice.model.enums.AccessMethodType;
 import com.ega.datarepositorysevice.repository.AccessMethodsRepository;
+import com.ega.datarepositorysevice.repository.ObjectRepository;
+import com.ega.datarepositorysevice.utils.TestObjectCreator;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,35 +31,115 @@ public class AccessMethodsServiceIntegrationTest {
 
      @Autowired
     private AccessMethodsRepository accessMethodsRepository;
+     @Autowired
+     private ObjectRepository objectRepository;
 
-    private AccessMethods accessMethodsTestObject;
+    private AccessMethods savedAccessMethodsTestObject;
+    private AccessMethods unsavedAccessMethodsTestObject;
+    private Object containingObject;
 
     @Before
     public void prepareDatabase() {
         Map<String, String> map = new HashMap<>();
         map.put("Authorization", "Basic Z2E0Z2g6ZHJz");
         AccessURL accessURL = new AccessURL(null,"https://www.youtube.com/watch?v=nsoIcQYlPxg", map);
-        accessMethodsTestObject = new AccessMethods(null, AccessMethodType.S3, "region", accessURL);
-        accessMethodsTestObject = accessMethodsRepository.save(accessMethodsTestObject);
+        savedAccessMethodsTestObject = new AccessMethods(null, AccessMethodType.S3, "region", accessURL);
+        unsavedAccessMethodsTestObject = new AccessMethods(null, AccessMethodType.S3, "region", accessURL);
+        containingObject = TestObjectCreator.getObject();
+        containingObject = objectRepository.save(containingObject);
+    }
+
+
+    @Test
+    public void testSaving(){
+        Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(containingObject.getId(),Mono.just(unsavedAccessMethodsTestObject));
+        AccessMethods accessMethods = accessMethodsMono.block();
+        Assert.assertNotNull(accessMethods.getAccessId());
+        Assert.assertEquals(unsavedAccessMethodsTestObject,accessMethods);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testSavingError(){
+        Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(0L,Mono.just(unsavedAccessMethodsTestObject));
+        AccessMethods accessMethods = accessMethodsMono.block();
+        Assert.assertNotNull(accessMethods.getAccessId());
+        Assert.assertEquals(unsavedAccessMethodsTestObject,accessMethods);
+    }
+
+    @Test
+    public void testUpdating(){
+        Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(containingObject.getId(),Mono.just(unsavedAccessMethodsTestObject));
+        AccessMethods accessMethods = accessMethodsMono.block();
+        Assert.assertNotNull(accessMethods.getAccessId());
+        Assert.assertEquals(unsavedAccessMethodsTestObject,accessMethods);
+        accessMethods.setRegion("new region");
+        Mono<AccessMethods> updatedAccessMethodsMono = accessMethodsService.updateAccessMethod(containingObject.getId(),Mono.just(accessMethods));
+        Assert.assertEquals(accessMethods,updatedAccessMethodsMono.block());
+    }
+
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdatingObjectNotFound(){
+        Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(containingObject.getId(),Mono.just(unsavedAccessMethodsTestObject));
+        AccessMethods accessMethods = accessMethodsMono.block();
+        Assert.assertNotNull(accessMethods.getAccessId());
+        Assert.assertEquals(unsavedAccessMethodsTestObject,accessMethods);
+        accessMethods.setRegion("new region");
+        Mono<AccessMethods> updatedAccessMethodsMono = accessMethodsService.updateAccessMethod(0L,Mono.just(accessMethods));
+        Assert.assertEquals(accessMethods,updatedAccessMethodsMono.block());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdatingAccessNotFound(){
+        Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(containingObject.getId(),Mono.just(unsavedAccessMethodsTestObject));
+        AccessMethods accessMethods = accessMethodsMono.block();
+        Assert.assertNotNull(accessMethods.getAccessId());
+        Assert.assertEquals(unsavedAccessMethodsTestObject,accessMethods);
+        accessMethods.setRegion("new region");
+        accessMethods.setAccessId(0L);
+        Mono<AccessMethods> updatedAccessMethodsMono = accessMethodsService.updateAccessMethod(containingObject.getId(),Mono.just(accessMethods));
+        Assert.assertEquals(accessMethods,updatedAccessMethodsMono.block());
+    }
+
+    @Test
+    public void testDeleting(){
+        Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(containingObject.getId(),Mono.just(unsavedAccessMethodsTestObject));
+        AccessMethods accessMethods = accessMethodsMono.block();
+        Assert.assertTrue(accessMethodsService.deleteById(containingObject.getId(),accessMethods.getAccessId()).block());
+        Assert.assertFalse(accessMethodsRepository.existsById(unsavedAccessMethodsTestObject.getAccessId()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeletingAccessMethodNotFound(){
+        Mono<Boolean> mono = accessMethodsService.deleteById(containingObject.getId(),0L);
+        mono.block();
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDeletingObjectNotFound(){
+        Mono<Boolean> mono = accessMethodsService.deleteById(0L,0L);
+        mono.block();
     }
 
     @Test
     public void testExistingValue() {
-        Mono<AccessMethods> accessMethod = accessMethodsService.getAccessMethodsById(accessMethodsTestObject.getAccessId());
+        Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(containingObject.getId(),Mono.just(unsavedAccessMethodsTestObject));
+        AccessMethods accessMethods = accessMethodsMono.block();
+        Mono<AccessMethods> accessMethod = accessMethodsService.getAccessMethodsById(containingObject.getId(),accessMethods.getAccessId());
         Optional<AccessMethods> accessMethodsOptional = accessMethod.blockOptional();
 
         Assert.assertTrue(accessMethodsOptional.isPresent());
-        Assert.assertEquals(accessMethodsTestObject, accessMethodsOptional.get());
+        Assert.assertEquals(savedAccessMethodsTestObject, accessMethodsOptional.get());
 
 
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void testEmptyValue() {
-        Mono<AccessMethods> accessMethod = accessMethodsService.getAccessMethodsById(accessMethodsTestObject.getAccessId()+1);
-        Optional<AccessMethods> accessMethodsOptional = accessMethod.blockOptional();
+        Mono<AccessMethods> accessMethod = accessMethodsService.getAccessMethodsById(containingObject.getId(), 0L);
+        accessMethod.block();
 
-        Assert.assertFalse(accessMethodsOptional.isPresent());
     }
 
 }
