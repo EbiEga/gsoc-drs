95283d0dcd72b4da07dffb7b51f0504e76c2f795
Dilschat <dilschat@yandex.ru>
2019-07-26 17:27:46 +0500

    accessMethodsHandler fixed

--- src/main/java/com/ega/datarepositorysevice/controller/handler/AccessMethodHandler.java
+++ src/main/java/com/ega/datarepositorysevice/controller/handler/AccessMethodHandler.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 import static com.ega.datarepositorysevice.controller.HandlerUtils.*;
 
@@ -23,44 +24,48 @@ public class AccessMethodHandler {
     }
 
     public Mono<ServerResponse> getAccess(ServerRequest request) {
-        try {
-            Error notFoundError = new Error("The requested AccessMethod wasn't found", HttpStatus.NOT_FOUND);
-            Mono<AccessMethods> accessMethodMono = accessMethodsService
-                    .getAccessMethodsById(retrievePathVariable(request, OBJECT_PATH_VARIABLE),retrievePathVariable(request, ACCESS_METHODS_PATH_VARIABLE));
-
-            return HandlerUtils.returnOkResponse(accessMethodMono, notFoundError);
-        } catch (IllegalArgumentException e) {
-            return HandlerUtils.returnBadRequest(e);
-        }
-
-
+        Mono<Tuple2<Long, Long>> parameters = Mono.zip(retrievePathVariable(request, OBJECT_PATH_VARIABLE), retrievePathVariable(request, ACCESS_METHODS_PATH_VARIABLE));
+        return parameters.flatMap(tuple -> {
+            Mono<AccessMethods> accessMethodsMono = accessMethodsService.getAccessMethodsById(tuple.getT1(), tuple.getT2());
+            return accessMethodsMono.flatMap(HandlerUtils::returnOkResponse)
+                    .onErrorResume(HandlerUtils::returnNotFound);
+        }).onErrorResume(HandlerUtils::returnBadRequest);
     }
 
     public Mono<ServerResponse> saveAccess(ServerRequest request) {
-        Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(retrievePathVariable(request, OBJECT_PATH_VARIABLE),request.bodyToMono(AccessMethods.class));
+        Mono<Long> monoLong = retrievePathVariable(request, OBJECT_PATH_VARIABLE);
+        return monoLong.flatMap(parameter -> {
+            Mono<AccessMethods> accessMethodsMono = accessMethodsService.saveAccessMethod(parameter, request.bodyToMono(AccessMethods.class));
+            return accessMethodsMono.flatMap(HandlerUtils::returnOkResponse)
+                    .onErrorResume(HandlerUtils::returnNotFound);
+        })
+                .onErrorResume(HandlerUtils::returnBadRequest);
 
-        return HandlerUtils
-                .returnOkResponse(accessMethodsMono);
 
     }
 
     public Mono<ServerResponse> deleteAccess(ServerRequest request) {
-        Mono<Void> deletedMono = accessMethodsService.deleteById(retrievePathVariable(request, OBJECT_PATH_VARIABLE),retrievePathVariable(request, ACCESS_METHODS_PATH_VARIABLE));
-        return deletedMono
-                .map(deleted -> HandlerUtils.returnOkResponse(Mono.empty()).block())
-                .onErrorResume(e -> returnBadRequest(e.getCause()));
+        Mono<Tuple2<Long, Long>> requestParameters = Mono.zip(retrievePathVariable(request, OBJECT_PATH_VARIABLE), retrievePathVariable(request, ACCESS_METHODS_PATH_VARIABLE));
+        return requestParameters.flatMap(parameters -> {
+            Mono<Void> accessMethods = accessMethodsService.deleteById(parameters.getT1(), parameters.getT2());
+            return accessMethods.flatMap(HandlerUtils::returnOkResponse)
+                    .onErrorResume(HandlerUtils::returnNotFound);
+        })
+                .onErrorResume(HandlerUtils::returnBadRequest);
     }
 
     public Mono<ServerResponse> updateAccess(ServerRequest request) {
-        try {
-            Error notFoundError = new Error("The requested AccessMethod wasn't found", HttpStatus.NOT_FOUND);
-
+        Mono<Tuple2<Long, Long>> requestParameters = Mono.zip(retrievePathVariable(request, OBJECT_PATH_VARIABLE), retrievePathVariable(request, ACCESS_METHODS_PATH_VARIABLE));
+        return requestParameters.flatMap(parameters -> {
             Mono<AccessMethods> accessMethodsMono = accessMethodsService
-                    .updateAccessMethod(retrievePathVariable(request, OBJECT_PATH_VARIABLE),request.bodyToMono(AccessMethods.class));
-            return HandlerUtils.returnOkResponse(accessMethodsMono, notFoundError);
-        }catch (IllegalArgumentException e){
-            return HandlerUtils.returnBadRequest(e);
-        }
-    }
+                    .updateAccessMethod(parameters.getT1(), request.bodyToMono(AccessMethods.class));
+            return accessMethodsMono.flatMap(HandlerUtils::returnOkResponse)
+                    .onErrorResume(HandlerUtils::returnNotFound);
+        })
+                .onErrorResume(HandlerUtils::returnBadRequest);
 
+
+    }
 }
+
+
